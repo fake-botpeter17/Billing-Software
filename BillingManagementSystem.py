a21c228a890ccf08e6858d52fd3d9a0978c77c2f
fake-botpeter17 <fake.botpeter17@gmail.com>
2024-02-09 18:11:10 +0530

    Defined Menu(), enhanced Auth(),Login(), Added feature ROLE

--- BillingManagementSystem.py
+++ BillingManagementSystem.py
@@ -1,8 +1,10 @@
+import datetime
 from psycopg2 import *
-#import pandas
+import pandas
+from random import randint
 #from tkinter import *
 #def authentication():
-#def Billing():
+
 #def Stock():
 #def Stock_Update():
 #def Search():
@@ -12,20 +14,46 @@ from math import *
 def Init():
     con=connect()
     cur = con.cursor()
-def Home():
+    Admin=False
+def Login():
     Uid=input("Enter User ID:")
     Pwd=input("Enter Password:")
-    if Auth(Uid,Pwd):
-        #Menu
-        #Try Different Menus for Admin and User
-    print("Wrong password")  #STOPPED HERE       
-
-    return 0
+    if not(Auth(Uid,Pwd)):
+        print("Email or password is invalid!")                                      #Displaying  error message when user enters wrong email id/password
+        Login()
+    Menu()      
 def Auth(a,b):
     cur.execute("select * from users")
     check=cur.fetchall()
     for  i in check:
-        if i[0]==i[1]:
+        if i[0]==i[1]:                                                          #Checking if the credentials are Correct 
+            if i[2]=="Admin":
+                Admin=True                                                      #Assigning the role (Admin or not)
             return True
     return False
+def Menu():
+    print("1. Billing \n2. Inventory \n3. Stock Management\n 4. Sales Info\n5. Staff Management")
+    ch=int(input(f"Please Enter Your Choice (1/2/3/4):"))
+    if ch==1:
+        Billing()
+    elif ch==2:
+        Inventory()
+    elif ch==3:
+        StockMngmnt()
+    elif ch==4:
+        SalesInfo()
+    elif ch==5:
+        StaffMngmnt()
+    else:
+        print("\nInvalid Option!!!\n")
+        Menu()
+
+def Billing():
+    cust_name=input("Customer Name:")
+    billno=randint(0,9999)
+    print(f"\nYour Bill Number is {billno}")
+    itemcode=input("Item Code:")
+    qty=int(input("Quantity:")) 
+    rate=float(input("Price per Item:"))
+    price=qty*rate
 
