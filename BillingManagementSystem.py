b22ade39d78df28091eea9d4c89bdf19e08943e1
fake-botpeter17 <fake.botpeter17@gmail.com>
2024-02-09 20:43:35 +0530

    Optimisation

--- BillingManagementSystem.py
+++ BillingManagementSystem.py
@@ -1,35 +1,37 @@
 import datetime
+#import pyarrow
 from psycopg2 import *
-import pandas
+#import pandas
 from random import randint
-#from tkinter import *
+from tkinter import *
 #def authentication():
-
 #def Stock():
 #def Stock_Update():
 #def Search():
+#from math import * 
 from time import sleep
-from math import * 
-
-def Init():
-    con=connect()
-    cur = con.cursor()
-    Admin=False
 def Login():
+    login_window=Tk()
+    login_window.title("Login")
+    login_window.geometry('300x150')
+    #login_window.configure(bg='light grey')
     Uid=input("Enter User ID:")
     Pwd=input("Enter Password:")
     if not(Auth(Uid,Pwd)):
         print("Email or password is invalid!")                                      #Displaying  error message when user enters wrong email id/password
         Login()
+    login_window.mainloop()
     Menu()      
 def Auth(a,b):
     cur.execute("select * from users")
     check=cur.fetchall()
     for  i in check:
-        if i[0]==i[1]:                                                          #Checking if the credentials are Correct 
-            if i[2]=="Admin":
-                Admin=True                                                      #Assigning the role (Admin or not)
-            return True
+        if i[0]==a:
+            if i[1]==b:
+                if i[2]=="Admin":
+                    global Admin
+                    Admin=True                                                      #Assigning the role (Admin or not)
+                    return True
     return False
 def Menu():
     print("1. Billing \n2. Inventory \n3. Stock Management\n 4. Sales Info\n5. Staff Management")
@@ -53,7 +55,18 @@ def Billing():
     billno=randint(0,9999)
     print(f"\nYour Bill Number is {billno}")
     itemcode=input("Item Code:")
+    
     qty=int(input("Quantity:")) 
     rate=float(input("Price per Item:"))
     price=qty*rate
+    #Should plan
+#def Inventory():
 
+
+
+con=connect (host='192.168.226.69', database = 'BMS',user = 'postgres',password = 'Nebinson@1', port = '5432')
+cur = con.cursor()
+Admin=False
+bye=False
+while not bye:
+    Login()
