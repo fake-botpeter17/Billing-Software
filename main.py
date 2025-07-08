7d6645db5e8bd97b8986b26a2b6a1c30afde8a76
Peter <fake.botpeter17@gmail.com>
2025-07-08 17:38:09 +0530

    Fixed: Resource Loading Issue in dependencies/

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from BillingManagementSystem import starter as bms
 from Resources.Resource import load_resource
 
 def main():
     load_resource()
+    from BillingManagementSystem import starter as bms
     bms()
 
 if __name__ == "__main__":
