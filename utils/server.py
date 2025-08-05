6e02b7af205b28a18a8ac5573da82debbd678b84
Peter <fake.botpeter17@gmail.com>
2025-08-05 16:55:22 +0530

    Removed recurrent functions and fixed naming.

--- utils/server.py
+++ utils/server.py
@@ -4,17 +4,13 @@ from os.path import join as pathJoiner
 from logging import error
 from pickle import dump,load
 
-def Set_api(api_key: str) -> bool:
+def set_api(api_key: str) -> bool:
     if api_key is None or api_key == "":
         raise ValueError("API key cannot be None or empty")
     with open(pathJoiner("Resources", "sak.dat"), 'wb') as file:
         data = api_key.encode("utf-32")
         dump(data, file)
     return True
-
-def view_API() -> str:
-    with open(pathJoiner("Resources", "sak.dat"), 'rb') as file:
-        return load(file).decode("utf-32")
 
 def run_check_server_periodically():
     def check_server_task():
